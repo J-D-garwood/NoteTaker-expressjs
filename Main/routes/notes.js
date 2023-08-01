ad76173214e01989727981f9cbdbe4977d636c2c
J_D_Nye <garwoodjdm@gmail.com>
2023-08-01 18:22:38 +1000

    notes routing expanded upon

--- Main/routes/notes.js
+++ Main/routes/notes.js
@@ -1,11 +1,27 @@
 const notes = require('express').Router();
+const uuid = require('../helpers/uuid');
+
+const { readFromFile } = require('../helpers/fsUtils')
 
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    res.send("poopypants")
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
     //insert read from File func
 });
 
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request recieved to add a note`);
+    
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        }
+    }
+})
 // insert notes.post thingy here
 
 module.exports = notes;
